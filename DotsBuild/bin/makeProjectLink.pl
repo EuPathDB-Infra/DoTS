5dfb858cf398fda000e2552cc725fc6ce9201796
Steve Fischer <sfischer@upenn.edu>
2002-12-16 21:48:48 +0000

    convert to gus 3.0
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/DoTS/trunk@2840 735e2a04-f8fc-0310-8a1b-f2942603c481

--- DotsBuild/bin/makeProjectLink.pl
+++ DotsBuild/bin/makeProjectLink.pl
@@ -6,35 +6,38 @@
 use strict;
 
 use Getopt::Long;
-use Objects::dbiperl_utils::DbiDatabase;
+use GUS::ObjRelP::DbiDatabase;
 
 
-my ($login,$password,$verbose,$database,$allgenes_num,$restart,$commit,$taxon);
+my ($gusConfigFile,$verbose,$allgenes_num,$restart,$commit,$imcloneDbRlsId,$taxon);
 &GetOptions("verbose!"=> \$verbose,
-            "login=s" => \$login,
-	    "password=s" => \$password,
-            "database=s" => \$database,
+	    "gusConfigFile=s" => \$gusConfigFile,
             "allgenes_num=s" => \$allgenes_num,
+            "imclone_db_rls_id=s" => \$imcloneDbRlsId,
 	    "restart!" => \$restart,
 	    "commit!" => \$commit,
 	    "taxon=i" => \$taxon);
 
-if(!$login || !$password || $database || $allgenes_num){
-	die "usage: makeProjectLink.pl --verbose --commit <commit inserts> --restart <optional, if restart required> --allgenes_num <version num of allgenes> --login [GUSrw] --database [GUSdev] --password <database password> --\n";
+if($allgenes_num){
+	die "usage: makeProjectLink.pl --verbose --commit <commit inserts> --restart <optional, if restart required> --allgenes_num <version num of allgenes> --gusConfigFile [$GUS_CONFIG_FILE] --\n";
 }
 
 
-$database = $database ? $database : "GUSdev";
-$login = $login ? $login : 'gusrw';
-
 print STDERR "Establishing dbi login\n" if $verbose;
-my $db = new DbiDatabase( undef, $login, $password, $verbose, 0, 1, $database );
+my $gusconfig = GUS::Common::GusConfig->new($gusConfigFile);
+
+my $db = GUS::ObjRelP::DbiDatabase->new($gusconfig->getDbiDsn(),
+					$gusconfig->getReadOnlyDatabaseLogin(),
+					$gusconfig->getReadOnlyDatabasePassword,
+					$verbose,0,1,
+					$gusconfig->getCoreSchemaName());
+
 
 my $dbh = $db->getQueryHandle();
 
 my $project_id = &getProject($dbh);
 
-my $idHash = &getHash($dbh,$taxon);
+my $idHash = &getHash($dbh,$taxon,$externalDbRlsId);
 
 &idsDone($dbh,$idHash,$project_id) if ($restart);
 
@@ -46,7 +49,7 @@ sub getProject {
     
     my ($db) = @_; 
 
-    my $sql = "select max(project_id) from project";
+    my $sql = "select max(project_id) from core.projectinfo";
     
     my $stmt = $db->prepareAndExecute($sql);
     
@@ -58,14 +61,14 @@ sub getProject {
     
     if (!$restart) {
 	$project++;
-	my $rows = $db->do("insert into Project Values ($project,$allgenes,$description,SYSDATE,1,1,1,1,1,0,12,0,0,0)");
+	my $rows = $db->do("insert into core.ProjectInfo Values ($project,$allgenes,$description,SYSDATE,1,1,1,1,1,0,12,0,0,0)");
 	if ($rows) {
-	    print STDERR ("Row inserted into Project table, project_id = $project\n");
+	    print STDERR ("Row inserted into core.ProjectInfo table, project_id = $project\n");
 	    $db->commit() if ($commit);
 	    
 	}
 	else {
-	    print STDERR ("Row insertion into Project table failed\n");
+	    print STDERR ("Row insertion into ProjectInfo table failed\n");
 	    exit;
 	}
     }
@@ -74,13 +77,25 @@ sub getProject {
 
 sub getHash {
     
-    my ($db, $taxon_id) = @_; 
+    my ($db, $taxon_id, $externalDbRlsId) = @_; 
 
-    my $sql = "select na_sequence_id from assembly where taxon_id in ($taxon_id) 
-            minus (select s.assembly_na_sequence_id from assemblysequence s, externalnasequence e 
-            where s.na_sequence_id = e.na_sequence_id and e.taxon_id in ($taxon_id) and e.external_db_id = 3892 
-            minus (select s.assembly_na_sequence_id from assemblysequence s, externalnasequence e 
-            where s.na_sequence_id = e.na_sequence_id and e.taxon_id in ($taxon_id) and e.external_db_id != 3892))";
+    # subtract away assemblies that are exclusively imclone ESTs.
+    my $sql = "
+select na_sequence_id
+from dots.assembly
+where taxon_id in ($taxon_id)
+minus (select s.assembly_na_sequence_id
+       from dots.assemblysequence s, dots.externalnasequence e
+       where s.na_sequence_id = e.na_sequence_id
+       and e.taxon_id in ($taxon_id)
+       and e.external_database_release_id = $imcloneDbRlsId
+       minus (select s.assembly_na_sequence_id
+              from dots.assemblysequence s, dots.externalnasequence e
+              where s.na_sequence_id = e.na_sequence_id
+              and e.taxon_id in ($taxon_id)
+              and e.external_database_release_id != $imcloneDbRlsId
+       )
+)";
 
     my $stmt = $db->prepareAndExecute($sql);
 
@@ -98,7 +113,11 @@ sub idsDone {
     
     my ($db,$hash,$project) = @_;
 
-    my $sql = "select id from projectlink where project_id = $project and table_id = 56";
+    my $sql = 
+"select id 
+ from core.projectlink 
+ where project_id = $project 
+ and table_id = 56";
 
     my $stmt = $db->prepareAndExecute($sql);
 
@@ -122,7 +141,7 @@ sub insertProjectLink {
 
     print STDERR ("$num assembly ids to be entered into ProjectLink\n");
 
-    my $sql = "insert into ProjectLink (ProjectLink_sq.nextval,$project,56,?,null,SYSDATE,1
+    my $sql = "insert into core.ProjectLink (ProjectLink_sq.nextval,$project,56,?,null,SYSDATE,1
 ,1,1,1,1,0,12,0,$project,0)";
 
     my $stmt = $db->prepare($sql);
