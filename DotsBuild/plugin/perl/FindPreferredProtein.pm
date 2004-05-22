07d9c79c6b3da341e5fc8ec5691d5607bbb830ce
Deborah Pinney <pinney@upenn.edu>
2004-05-22 17:52:01 +0000

    modify updateProteinInstance sub
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/DoTS/trunk@3290 735e2a04-f8fc-0310-8a1b-f2942603c481

--- DotsBuild/plugin/perl/FindPreferredProtein.pm
+++ DotsBuild/plugin/perl/FindPreferredProtein.pm
@@ -99,78 +99,96 @@ sub getAssemblies {
   while(my ($assembly) = $stmt->fetchrow_array()){
     $assemblies{$assembly} = 1;
   }
-
+  my $num = scalar (keys %assemblies);
+  $self->log ("$num DTs contain mRNA and will have preferred proteins assigned\n");
   return \%assemblies;
 }
 
 sub updateProteinInstance {
 
   my ($self,$dbh,$assemblies) = @_;
-
   my $refseq_db_rel = $self->getArgs()->{'refseq_db_rel_id'};
-
   my $swissprot_db_rel = $self->getArgs()->{'swissprot_db_rel_id'}; 
-
+  my $num;
   my $refseqquery = "select p.protein_instance_id from dots.assemblysequence ass, dots.rnafeature rf, dots.externalnasequence x, dots.translatedaafeature tf, dots.externalaasequence xa, dots.proteinInstance p  where ass.assembly_na_sequence_id = ? and ass.na_sequence_id = x.na_sequence_id and x.external_database_release_id = $refseq_db_rel and ass.na_sequence_id = rf.na_sequence_id and rf.na_feature_id = tf.na_feature_id and tf.aa_sequence_id = xa.aa_sequence_id and tf.aa_feature_id = p.aa_feature_id order by xa.length desc";
 
   my $stmt =  $dbh->prepare($refseqquery);
-
-  foreach my $id (keys %$assemblies) {
+  foreach my $id (keys %{$assemblies}) {
     $stmt->execute($id);
     my ($proteininstance) = $stmt->fetchrow_array();
     $stmt->finish();
-    $dbh->do("update dots.proteininstance set is_reference = 1 where protein_instance_id = $proteininstance") || die "Can't update dots.proteininstance row : protein_instance_id = $proteininstance\n";
-    delete($assemblies->{$id});
+    if ($proteininstance) {
+      $dbh->do("update dots.proteininstance set is_reference = 1 where protein_instance_id = $proteininstance")       || die "Can't update dots.proteininstance row : protein_instance_id = $proteininstance\n";
+      $num++;
+      delete($assemblies->{$id});
+    }
   }
 
-  $self->log("Committing updates for RefSeq containing assemblies\n") if $self->getArgs()->{'commit'} ;
+  $self->log("Committing updates for $num RefSeq containing assemblies\n") if $self->getArgs()->{'commit'} ;
   $dbh->commit() if $self->getArgs()->{'commit'} ;
-
+  $num = 0;
   my $swissprotquery = "select p.protein_instance_id from dots.assemblysequence ass, dots.rnafeature rf, dots.nrdbentry n, dots.translatedaafeature tf, dots.externalaasequence xa, dots.proteinInstance p  where ass.assembly_na_sequence_id = ? and ass.na_sequence_id = rf.na_sequence_id and rf.na_feature_id = tf.na_feature_id and tf.aa_sequence_id = xa.aa_sequence_id and xa.aa_sequence_id = n.aa_sequence_id and n.external_database_release_id = $swissprot_db_rel and tf.aa_feature_id = p.aa_feature_id order by xa.length desc";
 
   my $stmt =  $dbh->prepare($swissprotquery);
 
-  foreach my $id (keys %$assemblies) {
+  foreach my $id (keys %{$assemblies}) {
     $stmt->execute($id);
     my ($proteininstance) = $stmt->fetchrow_array();
     $stmt->finish();
-    $dbh->do("update dots.proteininstance set is_reference = 1 where protein_instance_id = $proteininstance") || die "Can't update dots.proteininstance row : protein_instance_id = $proteininstance\n";
-    delete($assemblies->{$id});
+     if ($proteininstance) {
+       $dbh->do("update dots.proteininstance set is_reference = 1 where protein_instance_id = $proteininstance")       || die "Can't update dots.proteininstance row : protein_instance_id = $proteininstance\n";
+       $num++;
+       delete($assemblies->{$id});
+     }
   }
 
-  $self->log("Committing updates for assemblies having mRNA with translations in SwissProt\n") if $self->getArgs()->{'commit'} ;
+  $self->log("Committing updates for $num assemblies having mRNA with translations in SwissProt\n") if $self->getArgs()->{'commit'} ;
   $dbh->commit() if $self->getArgs()->{'commit'} ;
-
+  $num = 0;
   my $lengthquery = "select p.protein_instance_id from dots.assemblysequence ass, dots.rnafeature rf, dots.translatedaafeature tf, dots.externalaasequence xa, dots.proteinInstance p  where ass.assembly_na_sequence_id = ? and ass.na_sequence_id = rf.na_sequence_id and rf.na_feature_id = tf.na_feature_id and tf.aa_sequence_id = xa.aa_sequence_id and tf.aa_feature_id = p.aa_feature_id order by xa.length desc";
 
   my $stmt =  $dbh->prepare($lengthquery);
 
-  foreach my $id (keys %$assemblies) {
+  foreach my $id (keys %{$assemblies}) {
     $stmt->execute($id);
     my ($proteininstance) = $stmt->fetchrow_array();
     $stmt->finish();
-    $dbh->do("update dots.proteininstance set is_reference = 1 where protein_instance_id = $proteininstance") || die "Can't update dots.proteininstance row : protein_instance_id = $proteininstance\n";
-    delete($assemblies->{$id});
+    if ($proteininstance) {
+      $dbh->do("update dots.proteininstance set is_reference = 1 where protein_instance_id = $proteininstance")       || die "Can't update dots.proteininstance row : protein_instance_id = $proteininstance\n";
+      $num++;
+      delete($assemblies->{$id});
+    }
   }
 
-  $self->log("Committing updates for assemblies having mRNA with the longest translations\n") if $self->getArgs()->{'commit'} ;
+  $self->log("Committing updates for $num assemblies having mRNA with the longest translations\n") if $self->getArgs()->{'commit'} ;
   $dbh->commit() if $self->getArgs()->{'commit'} ;
-
+  $num = 0;
   my $query = "select p.protein_instance_id from dots.assembly a, dots.rnafeature rf, dots.translatedaafeature tf, dots.proteinInstance p  where a.na_sequence_id = ? and a.na_sequence_id = rf.na_sequence_id and rf.na_feature_id = tf.na_feature_id and tf.aa_feature_id = p.aa_feature_id";
 
   my $stmt =  $dbh->prepare($query);
 
-  foreach my $id (keys %$assemblies) {
+  foreach my $id (keys %{$assemblies}) {
     $stmt->execute($id);
     my ($proteininstance) = $stmt->fetchrow_array();
     $stmt->finish();
-    $dbh->do("update dots.proteininstance set is_reference = 1 where protein_instance_id = $proteininstance") || die "Can't update dots.proteininstance row : protein_instance_id = $proteininstance\n";
-    delete($assemblies->{$id});
+    if ($proteininstance) {
+      $dbh->do("update dots.proteininstance set is_reference = 1 where protein_instance_id = $proteininstance")       || die "Can't update dots.proteininstance row : protein_instance_id = $proteininstance\n";
+      $num++;
+      delete($assemblies->{$id});
+    }
   }
 
-  $self->log("Committing updates for assemblies having mRNA without translations\n") if $self->getArgs()->{'commit'} ;
+  $self->log("Committing updates for $num assemblies having mRNA without translations\n") if $self->getArgs()->{'commit'} ;
   $dbh->commit() if $self->getArgs()->{'commit'} ;
 
+  $num = scalar (keys %{$assemblies});
+
+  if ($num > 0) {
+    $self->log ("The following DTs containing mRNA have not been assigned a preferred protein:\n");
+    foreach my $id (keys %{$assemblies}) {
+      $self->log ("    $id\n");
+    }
+  }
 }
 
 1;
