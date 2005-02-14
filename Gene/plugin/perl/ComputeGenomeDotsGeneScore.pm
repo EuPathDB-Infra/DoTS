fede95b53f2c8206c2365dcd6e58a170cb24229f
Deborah Pinney <pinney@upenn.edu>
2005-02-14 19:24:27 +0000

    corrected syntax error in # 4 EST ...
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/DoTS/trunk@3509 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Gene/plugin/perl/ComputeGenomeDotsGeneScore.pm
+++ Gene/plugin/perl/ComputeGenomeDotsGeneScore.pm
@@ -198,7 +198,7 @@ sub processRegion {
 	# 4. EST 5'-3' plot score
         #$self->log("\tEST plot score ...") if $self->getArg('debug');
 	#DoTS::Gene::ConfidenceScore::ESTPlotScore::setESTPlotScore($dbh, $gdg_tab, $gdt_tab,
-						       $gid, $chr_id, $cs_info);
+	#						    $gid, $chr_id, $cs_info);
 	# overall score
 	$self->log("\toverall score ...") if $self->getArg('debug');
 	DoTS::Gene::ConfidenceScore::Overall::setScore($dbh, $gdg_tab,
