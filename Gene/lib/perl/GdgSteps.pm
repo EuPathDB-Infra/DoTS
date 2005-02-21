229405fa664835be291ccb8cf34a2d2c01ff3f0f
yongchan <yongchan@_.com>
2005-02-21 16:49:46 +0000

    dumpGeneSeqs step bug fix
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/DoTS/trunk@3516 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Gene/lib/perl/GdgSteps.pm
+++ Gene/lib/perl/GdgSteps.pm
@@ -671,7 +671,8 @@ sub dumpGeneSeqs {
   my$outFile = "$pipelineDir/releasefiles/$sp/"
       . $mgr->{releaseFilePrefix} . $mgr->{releaseFileSuffix} . '.' . lc($type) . '.fa' ;
 
-  my $cmd = "dumpGeneSeqs $taxonId $genomeId $gusConfigFile 1 1 1 1 $outFile 2>>  $logFile";
+  my $cmd = "dumpGeneSeqs $taxonId $genomeId $gusConfigFile 1 1 1 1 "
+    . ($type =~ /^coding/i ? 1 : 0) . " $outFile 2>>  $logFile";
 
   $mgr->runCmd($cmd);
 
